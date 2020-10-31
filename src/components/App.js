12b56fa0d8cfea8d6ed3b9818a79b6cba5ab027a
Varun kumar <45607763+kickvire@users.noreply.github.com>
2020-10-31 14:30:02 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,69 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
     top: "0px",
+    position: "absolute"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  //const [style, setStyle] = useState(ballPosition);
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({
+      left: "0px",
+      top: "0px",
+      position: "absolute"
+    });
+  };
+  const start = () => {
+    setRenderBall(true);
+  };
+  const handleUserKeyPress = (event) => {
+    console.log(event.key);
+    let newx = x;
+    let newy = y;
+    if (event.key === "ArrowRight") {
+      newx = x + 5;
+      setX(newx);
+    } else if (event.key === "ArrowLeft") {
+      newx = x - 5;
+      setX(newx);
+    } else if (event.key === "ArrowUp") {
+      newy = y - 5;
+      setY(newy);
+    } else if (event.key === "ArrowDown") {
+      newy = y + 5;
+      setY(newy);
+    }
+    console.log(x + " " + y);
+    setBallPosition({
+      left: `${newx}px`,
+      top: `${newy}px`,
+      position: `absolute`
+    });
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", handleUserKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleUserKeyPress);
+    };
+  });
+
+  const renderChoice = () => {
+    if (!renderBall) {
+      return (
+        <button onClick={start} className="start">
+          Start
+        </button>
+      );
+    } else return <div style={ballPosition} className="ball"></div>;
+  };
 
   return (
     <div className="playground">
